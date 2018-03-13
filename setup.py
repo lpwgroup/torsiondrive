2b0f016bcec26531d2ff3d1ee9d1aed917657f93
qyd <saviorbruce@gmail.com>
2018-03-13 14:08:25 -0700

    add setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='crank',
+    description='Dihedral scanner with wave propagation',
+    url='https://github.com/lpwgroup/crank',
+    author='Yudong Qiu, Lee-Ping Wang',
+    packages=['crank'],
+    entry_points={'console_scripts': [
+        'crank-launch = crank.launch:main',
+    ]},
+    install_requires=[
+        'numpy>=1.11',
+        'geometric'
+    ])
