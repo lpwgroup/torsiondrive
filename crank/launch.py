3e0a9a8afa9132c96b988acbeb65f1a5c0537c9d
qyd <saviorbruce@gmail.com>
2018-05-02 12:30:48 -0700

    use absolute import for crank.WQtools

--- crank/launch.py
+++ crank/launch.py
@@ -33,7 +33,7 @@ def create_engine(enginename, inputfile=None, work_queue_port=None, native_opt=F
     engine_dict = {'psi4': EnginePsi4, 'qchem': EngineQChem, 'terachem':EngineTerachem}
     # initialize a work_queue
     if work_queue_port != None:
-        from WQtools import WorkQueue
+        from crank.WQtools import WorkQueue
         work_queue = WorkQueue(work_queue_port)
     else:
         work_queue = None
