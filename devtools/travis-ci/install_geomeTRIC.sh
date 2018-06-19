d8d049435f9fcd2638bb76dbd5f2bcdb620e0999
qyd <saviorbruce@gmail.com>
2018-06-19 14:58:44 -0700

    switch to the latest geomeTRIC

--- devtools/travis-ci/install_geomeTRIC.sh
+++ devtools/travis-ci/install_geomeTRIC.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/leeping/geomeTRIC/archive/0.8.2.tar.gz
-tar zxf 0.8.2.tar.gz
-cd geomeTRIC-0.8.2/
+git clone https://github.com/leeping/geomeTRIC.git
+cd geomeTRIC
 python setup.py install
 cd ..
 
