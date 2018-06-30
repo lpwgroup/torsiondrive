999ff9fef4cc7f0dc48f13b7885d900d9e9c9f1e
Daniel Smith <malorian@me.com>
2018-06-30 15:02:36 -0700

    API: Minor tweaks and readability improvements

--- crank/tests/test_stack_api.py
+++ crank/tests/test_stack_api.py
@@ -12,11 +12,13 @@ from crank import crankServerAPI
 try:
     import qcengine
     import psi4
-except:
+except ImportError:
     pass
+
 
 class SimpleServer:
     """ A simple server that interfaces with crank and geometric to do the dihedral scanning work flow """
+
     def __init__(self, xyzfilename, dihedrals, grid_spacing):
         self.M = geometric.molecule.Molecule(xyzfilename)
         self.dihedrals = dihedrals
@@ -34,39 +36,53 @@ class SimpleServer:
         5. Collect the results and put them into new json input dictionary
         6. Go back to step 2.
         """
+
         # step 1
-        crank_state = crankServerAPI.create_initial_api_input(dihedrals=self.dihedrals, grid_spacing=self.grid_spacing, elements=self.elements, init_coords=self.init_coords)
+        crank_state = crankServerAPI.create_initial_api_input(
+            dihedrals=self.dihedrals,
+            grid_spacing=self.grid_spacing,
+            elements=self.elements,
+            init_coords=self.init_coords)
+
         while True:
             # step 2
             next_jobs = crank_api(crank_state, verbose=True)
+
             # step 3
             if len(next_jobs) == 0:
                 print("Crank Scan Finished")
                 return crankServerAPI.collect_lowest_energies(crank_state)
+
             # step 4
             job_results = collections.defaultdict(list)
             for grid_id_str, job_geo_list in next_jobs.items():
                 for job_geo in job_geo_list:
                     dihedral_values = crankServerAPI.gridIDStr_to_dihedralValues(grid_id_str)
+
+                    # Run geometric
                     geometric_input_dict = self.make_geomeTRIC_input(dihedral_values, job_geo)
                     geometric_output_dict = geometric.run_json.geometric_run_json(geometric_input_dict)
+
+                    # Pull out relevevant data
                     final_geo = geometric_output_dict['final_molecule']['molecule']['geometry']
                     final_energy = geometric_output_dict['final_molecule']['properties']['return_energy']
+
                     # Note: the results should be appended in the same order as in the inputs
                     # It's not a problem here when running serial for loop
                     job_results[grid_id_str].append((job_geo, final_geo, final_energy))
+
             # step 5
             crank_state = crankServerAPI.update_crank_state(crank_state, job_results)
-            # step 6
-
 
     def make_geomeTRIC_input(self, dihedral_values, geometry):
         """ This function should be implemented on the server, that takes QM specs, geometry and constraint
         to generate a geomeTRIC json input dictionary"""
+
         constraints_string = "$set\n"
         for (d1, d2, d3, d4), v in zip(self.dihedrals, dihedral_values):
             # geomeTRIC use atomic index starting from 1
-            constraints_string += "dihedral %d %d %d %d %f\n" % (d1+1, d2+1, d3+1, d4+1, v)
+            constraints_string += "dihedral %d %d %d %d %f\n" % (d1 + 1, d2 + 1, d3 + 1, d4 + 1, v)
+
         qc_schema_input = {
             "schema_name": "qc_schema_input",
             "schema_version": 1,
@@ -76,11 +92,12 @@ class SimpleServer:
             },
             "driver": "gradient",
             "model": {
-                "method": "MP2",
-                "basis": "cc-pvdz"
+                "method": "SCF",
+                "basis": "STO-3G"
             },
             "keywords": {}
         }
+
         geometric_input_dict = {
             "schema_name": "qc_schema_optimization_input",
             "schema_version": 1,
@@ -91,6 +108,7 @@ class SimpleServer:
             },
             "input_specification": qc_schema_input
         }
+
         return geometric_input_dict
 
 
@@ -101,13 +119,17 @@ def test_stack_simpleserver():
     Test the stack of crank -> geomeTRIC -> qcengine -> Psi4
     """
     orig_path = os.getcwd()
+
     this_file_folder = os.path.dirname(os.path.realpath(__file__))
     test_folder = os.path.join(this_file_folder, 'files', 'hooh-simpleserver')
     os.chdir(test_folder)
-    simpleServer = SimpleServer('start.xyz', dihedrals=[[0,1,2,3]], grid_spacing=[30])
+
+    simpleServer = SimpleServer('start.xyz', dihedrals=[[0, 1, 2, 3]], grid_spacing=[60])
     lowest_energies = simpleServer.run_crank_scan()
+
     result_energies = [lowest_energies[grid_id] for grid_id in sorted(lowest_energies.keys())]
-    assert np.allclose(result_energies, [-151.17383,-151.17416,-151.17455,-151.17477,-151.17455,-151.17367,-151.17199,
-        -151.16962,-151.16686,-151.16424,-151.16236,-151.16167,-151.16236,-151.16424,-151.16686,-151.16962,-151.17199,
-        -151.17367,-151.17455,-151.17477,-151.17455,-151.17416,-151.17383,-151.17370][1::2], atol=1e-4)
+    assert np.allclose(
+        result_energies, [-148.76511761, -148.76018225, -148.7505629, -148.76018225, -148.76511761, -148.76501337],
+        atol=1e-4)
+
     os.chdir(orig_path)
