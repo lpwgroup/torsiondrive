f68b9c6ba95a3ad0be68812664f9127e92797da8
Zhiyi Wu <zhiyi.wu@bioch.ox.ac.uk>
2022-01-14 16:42:35 +0000

    Update qm_engine.py

--- torsiondrive/qm_engine.py
+++ torsiondrive/qm_engine.py
@@ -108,10 +108,7 @@ class QMEngine(object):
                 subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             except subprocess.CalledProcessError:
                 print('Run failed. Ignore the current run.')
-                with open('Failed', 'w') as f:
-                    f.write('')
-                for file in output_files:
-                    shutil.move(file, file + '.bk')
+                open('Failed', 'a').close()
         else:
             self.work_queue.submit(cmd, input_files, output_files)
 
